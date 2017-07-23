d66039b54f20675d0eca633b8cc3883db3ea1c26
Tom Palmer <tjpalmer@tjpalmer.com>
2017-07-22 20:48:15 -0700

    Physics!

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,5 @@
+import {Body, Bodies, Engine, World} from 'matter-js';
+
 import {
   // TODO Clean out unused.
   AmbientLight, BoxGeometry, BufferAttribute, BufferGeometry, DirectionalLight,
@@ -9,11 +11,21 @@ import {
 export class Game {
 
   constructor(canvas: HTMLCanvasElement) {
+    // World.
+    let engine = this.engine = Engine.create();
+    let box1 = this.box1 = Bodies.rectangle(20, 20, 10, 10);
+    let box2 = this.box2 = Bodies.rectangle(13, 40, 8, 8);
+    let ground = Bodies.rectangle(0, -5, 1000, 10, {isStatic: true});
+    World.add(engine.world, [box1, box2, ground]);
+    console.log(engine.world.gravity);
+    engine.world.gravity.y = -1;
+    engine.world.gravity.scale = 1e-4;
+    // View.
     this.canvas = canvas;
     let renderer = this.renderer = new WebGLRenderer({canvas});
     this.camera =
       new PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
-    this.camera.position.x = -5;
+    this.camera.position.set(-5, 2, 0);
     this.camera.lookAt(new Vector3());
     this.camera.updateProjectionMatrix();
     renderer.setSize(canvas.width, canvas.height);
@@ -33,25 +45,38 @@ export class Game {
     let geo = new BoxGeometry(1, 1, 1);
     let material = new MeshPhysicalMaterial({color: 0xFFBB33});
     let cube = this.cube = new Mesh(geo, material);
+    cube.position.set(box1.position.x, box1.position.y, 0).multiplyScalar(0.1);
     scene.add(cube);
-    let floor = new Mesh(
-      new BoxGeometry(10, 1, 1), new MeshPhysicalMaterial({color: 0x00FF88}),
+    let box2Mesh = this.box2Mesh = new Mesh(
+      new BoxGeometry(0.8, 0.8, 1), new MeshPhysicalMaterial({color:0xAA55AA}),
     );
-    floor.position.y = -5;
+    box2Mesh.position.set(
+      box2.position.x, box2.position.y, 0,
+    ).multiplyScalar(0.1);
+    scene.add(box2Mesh);
+    let floor = new Mesh(
+      new BoxGeometry(100, 1, 1), new MeshPhysicalMaterial({color: 0x00FF88}),
+    );
+    floor.position.y = -0.5;
     scene.add(floor);
-    let ceiling = floor.clone();
-    ceiling.position.y = 5;
-    scene.add(ceiling);
     // Input.
     window.addEventListener('keydown', event => this.onKey(event, true));
     window.addEventListener('keyup', event => this.onKey(event, false));
   }
+
+  box1: Body;
+
+  box2: Body;
+
+  box2Mesh: Mesh;
 
   camera: PerspectiveCamera;
 
   canvas: HTMLCanvasElement;
 
   cube: Mesh;
+
+  engine: Engine;
 
   onKey(event: KeyboardEvent, down: boolean) {
     this.keys.set(event.key, down);
@@ -79,7 +104,15 @@ export class Game {
 
   step = () => {
     window.requestAnimationFrame(this.step);
-    this.cube.rotation.z += 0.05;
+    Engine.update(this.engine);
+    this.cube.rotation.z = this.box1.angle;
+    this.cube.position.set(
+      this.box1.position.x, this.box1.position.y, 0,
+    ).multiplyScalar(0.1);
+    this.box2Mesh.rotation.z = this.box2.angle;
+    this.box2Mesh.position.set(
+      this.box2.position.x, this.box2.position.y, 0,
+    ).multiplyScalar(0.1);
     velocity.set(0, 0);
     if (this.keys.get('ArrowUp')) {
       velocity.y = 1;
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   entry: {
     app: './src/main.tsx',
     vendor: [
+      'matter-js',
       'three',
     ],
   },
